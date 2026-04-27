f1d35551914a4cd1c4bc4824f1d4a2e982a9d091
mamettdd <mamettdd@gmail.com>
2026-04-27 10:33:30 +0700

    Update consumer_to_hdfs.py

--- kafka/consumer_to_hdfs.py
+++ kafka/consumer_to_hdfs.py
@@ -1,10 +1,8 @@
-# [Nama Anggota]: consumer_to_hdfs.py
 from kafka import KafkaConsumer
 from hdfs import InsecureClient
 import json
 from datetime import datetime
 
-# Inisialisasi Consumer
 consumer = KafkaConsumer(
     'airquality-api', 'airquality-rss',
     bootstrap_servers=['localhost:9092'],
@@ -12,7 +10,6 @@ consumer = KafkaConsumer(
     value_deserializer=lambda x: json.loads(x.decode('utf-8'))
 )
 
-# Inisialisasi HDFS Client (Port 9870 sesuai docker-compose)
 client = InsecureClient('http://localhost:9870', user='root')
 
 print("Consumer HDFS berjalan... Menunggu pesan...")
@@ -23,11 +20,9 @@ try:
         data = message.value
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         
-        # Buat path folder di HDFS
         hdfs_folder = f"/data/airquality/{topic.split('-')[1]}"
         hdfs_path = f"{hdfs_folder}/data_{timestamp}.json"
         
-        # Simpan ke HDFS
         with client.write(hdfs_path, encoding='utf-8') as writer:
             json.dump(data, writer)
             
